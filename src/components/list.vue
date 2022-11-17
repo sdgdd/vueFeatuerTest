08a4fb2609e7c26d3ec4c86719ce7e2956a20671
hhc <1173366497@qq.com>
2022-11-18 00:09:15 +0800

    feat: toList 组合式

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,32 +1,89 @@
 <template>
-  <input type="text" v-model="title" @keydown.enter="addList" />
+  <input type="text" v-model="title" @keydown.enter="addList" class="input" />
+  <span>{{ all }}/{{ active }}</span>
+  <div>
+    <button v-if="doList.length" @click="handleClear">清理</button>
+  </div>
+  <div>
+    <input type="checkbox" v-model="alldone" />
+    <span>全选</span>
+  </div>
   <ul v-if="doList.length">
     <li v-for="(item, index) of doList" :key="item.title">
       <input v-model="item.done" type="checkbox" />
       <span :class="{ done: item.done }">{{ item.title }}</span>
-      <Delete class="iconStyle" @click="deletList(index)">{{ item.title }}</Delete>
+      <Delete class="iconStyle" @click="deletList(index)" v-show="item.done">{{
+        item.title
+      }}</Delete>
     </li>
   </ul>
+  <div v-else>暂无数据</div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+let {
+  title,
+  doList,
+  all,
+  active,
+  alldone,
+  deletList,
+  addList,
+  handleClear,
+  selectAll,
+} = useTos();
+</script>
 
-let title = ref();
-let doList = ref([{ title: "学习乒乓", done: false }]);
-
-function deletList(index) {
-  console.log(index);
-  doList.value.splice(index, 1);
-}
-
-function addList() {
-  if (!title.value) return;
-  doList.value.push({
-    title: title.value,
-    done: false,
+<script>
+function useTos() {
+  let title = ref();
+  let doList = ref([{ title: "学习乒乓", done: false }]);
+  let all = computed(() => doList.value.length);
+  let active = computed(() => doList.value.filter((item) => item.done).length);
+  let alldone = computed({
+    get() {
+      return doList.value.length && doList.value.every((item) => item.done);
+    },
+    set(value) {
+      return doList.value.forEach((item) => {
+        item.done = value;
+      });
+    },
   });
-  title.value = "";
+
+  function deletList(index) {
+    doList.value.splice(index, 1);
+  }
+
+  function addList() {
+    if (!title.value) return;
+    doList.value.push({
+      title: title.value,
+      done: false,
+    });
+    title.value = "";
+  }
+
+  function handleClear() {
+    doList.value = doList.value.filter((item) => !item.done);
+  }
+
+  function selectAll() {
+    doList.value.forEach((item) => (item.done = true));
+  }
+
+  return {
+    title,
+    doList,
+    all,
+    active,
+    alldone,
+    deletList,
+    addList,
+    handleClear,
+    selectAll,
+  };
 }
 </script>
 
@@ -47,5 +104,9 @@ li {
   margin-left: 8px;
   width: 20px;
   height: 20px;
+}
+
+.input {
+  margin: 20px 20px 20px;
 }
 </style>
